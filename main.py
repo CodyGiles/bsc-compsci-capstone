1cfafc8580eca34ce2038d4439a8f77b9ff8a90f
Cody Giles <hello@codygiles.com>
2024-08-19 14:57:07 -0600

    Create actor's score feature

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # Cody Giles - Student ID: 010506641
 # C964 Capstone - Movie Audience Rating Predictor aka The MARP
 
-import csv
+# import csv
 import pandas as pd
 
-df = pd.read_csv('path_to_file.csv')
+# Bring in the dataset CSV
+dfCSV = pd.read_csv('imdb_movies - test.csv')
+
+dfCSV = pd.DataFrame(
+    {'test': []}
+)
+
+print(dfCSV)
